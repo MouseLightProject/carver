428dceb99a01ec7c980da74b17828cc9ae995440
Erhan Bas <erhanbas@gmail.com>
2017-09-15 17:13:03 -0400

    added readers for swc and transform added nano/micro to voxel conversion

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import util
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 def crop_from_render():
     inputtif = "/groups/mousebrainmicro/home/base/CODE/MOUSELIGHT/compression/compression/testin.tif"
@@ -9,4 +12,20 @@ def crop_from_render():
     # tif2mj2(inputtif,mj2file)
 
 if __name__ == '__main__':
-    crop_from_render()
+
+    import importlib
+    importlib.reload(util)
+
+    A = util.readTransfrom()
+    (um, edges, R, offset, scale) = util.readSWC(scale=1/1000)
+    # return (np.dot(np.linalg.pinv(A), um.T))
+    xyz = util.um2pix(um,A)
+    ## plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(xyz[0],xyz[1],xyz[2],'r.')
+
+
+
+
+
--- util.py
+++ util.py
@@ -26,10 +26,36 @@ def readTransfrom(transformfile = "/nrs/mouselight/SAMPLES/2017-06-10/transform.
                 # normalize diagonal with level
                 np.fill_diagonal(A,A.diagonal()/(2**(num-1)))
     return A
+def readSWC(swcfile='/groups/mousebrainmicro/mousebrainmicro/cluster/Tracings/2017-06-10_G-029_Consensus.swc',scale=1):
+    swcline=[]
+    offset = np.zeros((1,3))
+    offsetkey = 'OFFSET'
+    with open(swcfile, 'r') as f:
+        while True:
+            text = f.readline()
+            if not text: break
+            if text[0]=='#':
+                # check offset
+                if text[2:len(offsetkey)+2]==offsetkey:
+                    offset = np.array(text[len(offsetkey) + 3:-1].split(), dtype=np.float).reshape(1,3)
+                else:
+                    continue #header
+            else:
+                parts = text.split(' ')
+                swcline.append(parts)
+    lines = np.array(swcline, dtype=float).reshape(-1, 7)
+    edges = lines[:,(0,6)]
+    R = lines[:,5]
+    xyz = lines[:,2:5]
+    xyz = xyz + offset
+    xyz = xyz/scale
+    return (xyz,edges,R,offset,scale)
 
 def um2pix(um,A):
     # applies transform to convert um into pix location
-    return(np.dot(np.linalg.pinv(A),um))
+    # um_ = np.concatenate((um, np.ones((um.shape[0], 1))), axis=1)
+    # return(np.dot(np.linalg.pinv(A),um.T))
+    return(np.dot(np.diag(1 / np.diagonal(A[:3, :3])), (um - A[:, 3]).T))
 def pix2um(xyz,A):
     return(np.dot(A,xyz))
 
