fc07e8be4186b153900c6645a520d0dc846b96fd
erhanbas <erhanbas@gmail.com>
2018-02-05 15:02:37 -0500

    cleaned up main TODO: hook to segmentation

--- main.py
+++ main.py
@@ -4,35 +4,10 @@ import matplotlib.pyplot as plt
 import improc
 import cropper
 
-
-from scipy.spatial import distance as dist
-from morphsnakes import tests
-from morphsnakes import morphsnakes as morph
-
-
-from mpl_toolkits.mplot3d import Axes3D
-
 def sample_spherical(npoints, ndim=3):
     vec = np.random.randn(ndim, npoints)
     vec /= np.linalg.norm(vec, axis=0)
     return vec
-
-def snake(img):
-    tests.test_confocal3d()
-
-    macwe = morph.MorphACWE(img, smoothing=1, lambda1=1, lambda2=2)
-    macwe.levelset = morph.circle_levelset(img.shape, (30, 50, 80), 25)
-
-    viz = 1;
-    if viz:
-        fig = plt.figure(frameon=False)
-    for i in range(100):
-        macwe.step()
-        if viz:
-            fd = macwe.levelset
-            plt.imshow(np.max(img, axis=2), cmap='gray')
-            plt.imshow(np.max(fd, axis=2), cmap='jet', alpha=0.3)
-            plt.show();plt.pause(.1);plt.draw()
 
 if __name__ == '__main__':
 
@@ -48,8 +23,5 @@ if __name__ == '__main__':
     swcfolder = '/groups/mousebrainmicro/home/base/CODE/MOUSELIGHT/navigator/recon_repo'
     swcname = '2017-11-17_G-017_Seg-1'
     cropper.crop_from_render(swcfolder,swcname,datafold,outfolder)
-    # volumetric segmentation around reconstruction
-
-    tests.test_confocal3d()
 
 
