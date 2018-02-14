8f6293ad172693fd3c438c5e6ba5f73e8f100b3d
erhanbas <erhanbas@gmail.com>
2018-02-14 12:23:04 -0500

    added setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
-
 from distutils.core import setup
-
 setup(name='navigator',
       version='1.0',
       description='Navigates in an octree data folder structure and crops images based on input swc file',
       author='Erhan Bas',
       author_email='erhanbas@gmail.com',
       url='https://www.python.org/sigs/distutils-sig/',
-      packages=['distutils', 'distutils.command'],
-     )
+      py_modules = ['util', 'improc', 'cropper']
+      )
