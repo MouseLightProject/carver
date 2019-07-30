e5351b0fd66a9da647289f68fc1f8de0433399e2
Adam L. Taylor <taylora@janelia.hhmi.org>
2019-07-30 17:53:13 -0400

    Added a bunch of per-sample high-level scripts

---
+++ run_navigator_on_2018_07_02.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+
+import navigator
+
+navigator.main(['-i', '/nrs/mouselight/SAMPLES/2018-07-02',
+                '-s', '/groups/mousebrainmicro/mousebrainmicro/scripts/gt/2018-07-02/consensus-neurons-with-machine-centerpoints-labelled-as-swcs',
+                '-o', '/nrs/funke/mouselight-v2/2018-07-02'])
