d053f446ab705422a3cdf1979f7550a4e47519c6
Adam L. Taylor <taylora@janelia.hhmi.org>
2019-08-26 11:05:09 -0400

    Code used to check for off-by-one error in recent samples

--- read_all_fragment_centerpoints.m
+++ read_all_fragment_centerpoints.m
@@ -4,6 +4,7 @@ function result = read_all_fragment_centerpoints(fragments_folder_path)
     fragment_count = length(fragment_file_names)
     raw_result = zeros(10e6, 3) ;
     centerpoint_count = 0 ;
+    pbo = progress_bar_object(fragment_count) ;
     for fragment_index = 1 : fragment_count ,
         fragment_file_name = fragment_file_names{fragment_index} ;
         fragment_file_path = fullfile(fragments_folder_path, fragment_file_name) ;
@@ -15,17 +16,7 @@ function result = read_all_fragment_centerpoints(fragments_folder_path)
         fragment_centerpoint_count = size(fragment_centerpoints, 1) ;
         raw_result(centerpoint_count+1:centerpoint_count+fragment_centerpoint_count, :) = fragment_centerpoints ;
         centerpoint_count = centerpoint_count + fragment_centerpoint_count ;        
-        if mod(fragment_index, 100) == 0 ,
-            fprintf('.') ;
-            if mod(fragment_index, 10000) == 0 ,
-                fprintf('\n') ;
-            end
-        end
-    end
-    % Print a newline, unless we just printed one
-    if mod(fragment_count, 100000) ~= 0 ,
-        fprintf('\n') ;
-    end
-    
+        pbo.update(fragment_index) ;
+    end    
     result = raw_result(1:centerpoint_count,:) ;
 end
